fe5f1c96d664f76b62b44b81cf313821b230fb25
kepler17 <kep171024@gmail.com>
2025-01-17 22:49:08 +0100

    Minor changes to the saving dialog

--- src/main/java/myapps/SalvaPDFController.java
+++ src/main/java/myapps/SalvaPDFController.java
@@ -1,5 +1,26 @@
 package myapps;
 
-public class SalvaPDFController {
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
 
+public class SalvaPDFController {
+    FileChooser fileChooser = new FileChooser();
+
+
+
+    public SalvaPDFController(){
+
+    }
+
+    @FXML public void initialize(){
+
+    }
+
+    public String createHTML(Analisi currentAnalisi){
+        return "";
+    }
+
+    public void toPDF(){
+
+    }
 }
