6c9564bd3a24e3d4261b5db1403deb54c403cc00
kepler17 <kep171024@gmail.com>
2025-01-24 16:38:52 +0100

    Class: PesoCampione added

---
+++ src/main/java/myapps/datamodel/PesoCampione.java
@@ -0,0 +1,22 @@
+package myapps.datamodel;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+public class PesoCampione {
+
+    // hello
+    private final SimpleObjectProperty<Double> pesoCampione = new SimpleObjectProperty<>(0.0);
+
+    public SimpleObjectProperty<Double> getPesoCampione() {
+        return pesoCampione;
+    }
+
+    public void updatePesoCampione(double delta){
+        pesoCampione.setValue(pesoCampione.getValue() + delta);
+    }
+
+
+    public Double getPercentage(double value){
+        return (value / pesoCampione.getValue()) * 100;
+    }
+}
